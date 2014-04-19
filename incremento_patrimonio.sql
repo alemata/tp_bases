01958cc5a2dc344b08db45bdf24212836c46d878
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2014-04-19 19:53:33 -0300

    Add Incremento patrimonio

---
+++ incremento_patrimonio.sql
@@ -0,0 +1,11 @@
+# Probando con otra cosita desde el primero
+SELECT c.*, (df.patrimonio / di.patrimonio - 1) * 100 incremento_porcentual
+FROM declaraciones_juradas di
+INNER JOIN ciudadanos c ON c.id = di.ciudadano_id
+INNER JOIN declaraciones_juradas df ON di.ciudadano_id = df.ciudadano_id
+WHERE df.año = (
+	SELECT MAX(año) 
+	FROM declaraciones_juradas dj 
+	WHERE dj.ciudadano_id = di.ciudadano_id LIMIT 1)
+GROUP BY di.ciudadano_id
+ORDER BY di.año ASC;

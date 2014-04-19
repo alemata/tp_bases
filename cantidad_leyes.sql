ff86a5131303c7e35f031e740604ccdff81ed763
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2014-04-19 17:36:31 -0300

    Enhance query

--- cantidad_leyes.sql
+++ cantidad_leyes.sql
@@ -1,10 +1,5 @@
-(SELECT sesiones.id, count(*)
-FROM sesiones INNER JOIN leyes ON sesiones.id = leyes.sesion_id
+SELECT sesiones.id, count(leyes.id) cantidad
+FROM sesiones 
+LEFT JOIN leyes ON sesiones.id = leyes.sesion_id
 WHERE year(fecha_inicio) >= year(now()) - 2
-GROUP BY sesiones.id)
-UNION 
-(SELECT sesiones.id, 0
-FROM sesiones LEFT JOIN leyes ON sesiones.id = leyes.sesion_id
-WHERE leyes.id is NULL
-AND year(fecha_inicio) >= year(now()) - 2
-GROUP BY sesiones.id)
+GROUP BY sesiones.id;

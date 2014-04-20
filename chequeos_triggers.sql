6c2457734003e07f7ff62fd61dc59652748be7c2
alemata <amataloni@gmail.com>
2014-04-20 19:46:47 -0300

    Agregado triggers para asistencia
    Se asume que no puede estar presente en una votación y ausente en otra.

--- chequeos_triggers.sql
+++ chequeos_triggers.sql
@@ -10,7 +10,7 @@ CREATE TRIGGER chequear_edad_diputados
      END;
 $$
 
-DELIMITER $$
+DELIMITER $$ 
  
 CREATE TRIGGER chequear_edad_senadores
      BEFORE INSERT ON representaciones FOR EACH ROW
@@ -21,3 +21,40 @@ CREATE TRIGGER chequear_edad_senadores
      	END IF;
      END;
 $$
+
+DELIMITER $$
+
+CREATE TRIGGER no_agregar_asistencia_si_voto_ausente
+     BEFORE INSERT ON asistencias FOR EACH ROW
+     BEGIN
+          IF (SELECT count(*) 
+          		FROM votos 	
+          		WHERE ciudadano_id = NEW.ciudadano_id AND
+          		sesion_id = NEW.sesion_id AND
+          		tipo_de_voto = 'ausente' 
+              ) > 0 THEN 
+     		SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Hay un voto ausente para ese ciudadano en esa sesion';
+     	END IF;
+     END;
+$$
+
+DELIMITER $$
+
+CREATE TRIGGER no_agregar_voto_ausente_si_asistencia
+     BEFORE INSERT ON votos FOR EACH ROW
+     BEGIN
+          IF (NEW.tipo_de_voto = 'ausente' AND
+             (SELECT count(*) 
+          		FROM asistencias 	
+          		WHERE ciudadano_id = NEW.ciudadano_id AND
+          		sesion_id = NEW.sesion_id
+              ) > 0) THEN 
+     		SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'No se puede agregar un voto ausente porque hay datos que verifican que el legislador estuvo presente en esa sesion';
+     	END IF;
+     END;
+$$
+
+
+

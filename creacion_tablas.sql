48a1148c2cf10a0b7646ee917871565870a33c49
eMancu <emiliano.mancuso@gmail.com>
2014-04-23 21:43:59 -0300

    Create VIEW on creacion_tablas.sql

--- creacion_tablas.sql
+++ creacion_tablas.sql
@@ -253,5 +253,8 @@ SELECT ciudadano_id, año, COALESCE(SUM(valor),0) patrimonio
 FROM bienes_economicos
 GROUP BY año, ciudadano_id;
 
-
-
+CREATE VIEW voto_proyectos_que_pertenece AS
+SELECT i.diputado_id, p.proyecto_de_ley_id, v.tipo_de_voto
+FROM integrantes_comisiones i
+JOIN proyectos_de_ley_comisiones p ON i.comision_id = p.comision_id
+JOIN votos v ON v.ciudadano_id = i.diputado_id AND v.proyecto_de_ley_id = p.proyecto_de_ley_id;
--- diputados_con_voto.sql
+++ diputados_con_voto.sql
@@ -1,11 +1,3 @@
--- Simplifico todo en una sola vista
-CREATE VIEW voto_proyectos_que_pertenece AS
-SELECT i.diputado_id, p.proyecto_de_ley_id, v.tipo_de_voto
-FROM integrantes_comisiones i
-JOIN proyectos_de_ley_comisiones p ON i.comision_id = p.comision_id
-JOIN votos v ON v.ciudadano_id = i.diputado_id AND v.proyecto_de_ley_id = p.proyecto_de_ley_id;
-
-
 SELECT DISTINCT c.*
 FROM ciudadanos c
 INNER JOIN voto_proyectos_que_pertenece p ON c.id = p.diputado_id

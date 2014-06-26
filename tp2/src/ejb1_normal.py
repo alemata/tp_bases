6394d600582c9624efe7009608d6d09d87950f66
alemata <amataloni@gmail.com>
2014-06-26 18:54:13 -0300

    Agregado group estimator a los graficos

--- tp2/src/ejb1_normal.py
+++ tp2/src/ejb1_normal.py
@@ -2,6 +2,7 @@
 from distribution_steps import DistributionSteps
 from classic_histogram import ClassicHistogram
 from real_histogram import RealHistogram
+from estimator_group import EstimatorGroup
 import math
 
 import sqlite3
@@ -29,29 +30,32 @@ table = 'normal'
 columns = []
 max_errors_classic = []
 max_errors_steps = []
+max_errors_group = []
 param = 4
 for n in range(1, 11):
   column = 'd' + str(n)
   cEstimator = ClassicHistogram(db_name, table, column, param)
   rEstimator = RealHistogram(db_name, table, column, param)
   sEstimator = DistributionSteps(db_name, table, column, param)
+  gEstimator = EstimatorGroup(db_name, table, column, {'size': param, 'variance': 0.018})
 
   points = []
   classic_errors = []
   step_errors = []
+  group_errors = []
   for x in range(rEstimator.min_value, rEstimator.max_value + 1):
     ce = cEstimator.estimate_equal(x)
     se = sEstimator.estimate_equal(x)
     re = rEstimator.estimate_equal(x)
-    # print "CL:Sel(=%d) : %3.2f" % (x, ce)
-    # print "ST:Sel(=%d) : %3.2f" % (x, se)
-    # print "RE:Sel(=%d) : %3.2f \n" % (x, re)
+    ge = gEstimator.estimate_equal(x)
     points.append(x)
     classic_errors.append(abs(ce - re))
     step_errors.append(abs(se - re))
+    group_errors.append(abs(ge - re))
 
   max_errors_classic.append(max(classic_errors))
   max_errors_steps.append(max(step_errors))
+  max_errors_group.append(max(group_errors))
 
   columns.append(n)
 
@@ -61,12 +65,13 @@ import matplotlib.pyplot as plt
 
 a = plt.plot(columns, max_errors_classic, 'ro')
 b = plt.plot(columns, max_errors_steps, 'bo')
+c = plt.plot(columns, max_errors_group, 'g^')
 # Pad margins so that markers don't get clipped by the axes
 plt.margins(0.2)
 # Tweak spacing to prevent clipping of tick-labels
 plt.subplots_adjust(bottom=0.15)
 plt.xticks(columns)
-plt.legend( (a[0], b[0]), ('Classic', 'Steps') )
+plt.legend( (a[0], b[0], c[0]), ('Classic', 'Steps', 'Group') )
 plt.ylabel('Maximo error')
 plt.xlabel('Numero de columna')
 plt.show()
--- tp2/src/ejb1_uniforme.py
+++ tp2/src/ejb1_uniforme.py
@@ -2,6 +2,7 @@
 from distribution_steps import DistributionSteps
 from classic_histogram import ClassicHistogram
 from real_histogram import RealHistogram
+from estimator_group import EstimatorGroup
 
 import sqlite3
 import numpy as np
@@ -28,26 +29,32 @@ table = 'uniforme'
 columns = []
 max_errors_classic = []
 max_errors_steps = []
+max_errors_group = []
 param = 4
 for n in range(1, 11):
   column = 'd' + str(n)
   cEstimator = ClassicHistogram(db_name, table, column, param)
   rEstimator = RealHistogram(db_name, table, column, param)
   sEstimator = DistributionSteps(db_name, table, column, param)
+  gEstimator = EstimatorGroup(db_name, table, column, {'size': param, 'variance': 0.01})
 
   points = []
   classic_errors = []
   step_errors = []
+  group_errors = []
   for x in range(rEstimator.min_value, rEstimator.max_value + 1):
     ce = cEstimator.estimate_equal(x)
     se = sEstimator.estimate_equal(x)
     re = rEstimator.estimate_equal(x)
+    ge = gEstimator.estimate_equal(x)
     points.append(x)
     classic_errors.append(abs(ce - re))
     step_errors.append(abs(se - re))
+    group_errors.append(abs(ge - re))
 
   max_errors_classic.append(max(classic_errors))
   max_errors_steps.append(max(step_errors))
+  max_errors_group.append(max(group_errors))
 
   columns.append(n)
 
@@ -57,13 +64,14 @@ import matplotlib.pyplot as plt
 
 a = plt.plot(columns, max_errors_classic, 'ro')
 b = plt.plot(columns, max_errors_steps, 'bo')
+c = plt.plot(columns, max_errors_group, 'g^')
 # Pad margins so that markers don't get clipped by the axes
 plt.margins(0.2)
 # Tweak spacing to prevent clipping of tick-labels
 plt.yticks([0,0.1,0.2,0.3, 0.4])
 plt.subplots_adjust(bottom=0.15)
 plt.xticks(columns)
-plt.legend( (a[0], b[0]), ('Classic', 'Steps') )
+plt.legend( (a[0], b[0], c[0]), ('Classic', 'Steps', 'Group') )
 plt.ylabel('Maximo error')
 plt.xlabel('Numero de columna')
 plt.show()

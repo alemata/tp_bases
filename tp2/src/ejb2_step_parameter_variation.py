519806e23ee3a11b82f708d90e96deefcffff291
alemata <amataloni@gmail.com>
2014-06-26 20:33:15 -0300

    Ejb2 step con varias columnas.

--- tp2/src/ejb2_step_parameter_variation.py
+++ tp2/src/ejb2_step_parameter_variation.py
@@ -12,23 +12,26 @@ db_name = 'db.datos_normal_uniforme'
 # ------------------- UNIFORME ----------------------
 table = 'uniforme'
 columns = []
+error_avgs_col = []
 error_avgs = []
-for n in range(1, 11):
-  # Uniforme
-  column = 'd1'
-  sEstimator = DistributionSteps(db_name, table, column, parameter=n)
-  rEstimator = RealHistogram(db_name, table, column, parameter=n)
+for par in range(1, 11):
+  for n in range(1, 11):
+    column = 'd' + str(n)
+    sEstimator = DistributionSteps(db_name, table, column, parameter=par)
+    rEstimator = RealHistogram(db_name, table, column, parameter=par)
+    error_avgs_col = []
 
-  errors = []
-  for x in range(rEstimator.min_value, rEstimator.max_value + 1):
-    se = sEstimator.estimate_equal(x)
-    re = rEstimator.estimate_equal(x)
-    errors.append(abs(se - re))
+    errors = []
+    for x in range(rEstimator.min_value, rEstimator.max_value + 1):
+      se = sEstimator.estimate_equal(x)
+      re = rEstimator.estimate_equal(x)
+      errors.append(abs(se - re))
 
-  avg_classic_diff = sum(errors) / len(errors)
+    avg_classic_diff = sum(errors) / len(errors)
+    error_avgs_col.append(avg_classic_diff)
 
-  columns.append(n)
-  error_avgs.append(avg_classic_diff)
+  columns.append(par)
+  error_avgs.append(sum(error_avgs_col) / float(len(error_avgs_col)))
 
 
 import matplotlib.pyplot as plt
@@ -40,35 +43,38 @@ a = plt.plot(columns, error_avgs, 'ro')
 # ------------------- NORMAL ----------------------
 table = 'normal'
 columns = []
+error_avgs_col = []
 error_avgs = []
-for n in range(1, 11):
-  # Uniforme
-  column = 'd1'
-  sEstimator = ClassicHistogram(db_name, table, column, parameter=n)
-  rEstimator = RealHistogram(db_name, table, column, parameter=n)
+for par in range(1, 11):
+  for n in range(1, 11):
+    column = 'd' + str(n)
+    sEstimator = DistributionSteps(db_name, table, column, parameter=par)
+    rEstimator = RealHistogram(db_name, table, column, parameter=par)
+    error_avgs_col = []
 
-  errors = []
-  for x in range(rEstimator.min_value, rEstimator.max_value + 1):
-    se = sEstimator.estimate_equal(x)
-    re = rEstimator.estimate_equal(x)
-    errors.append(abs(se - re))
+    errors = []
+    for x in range(rEstimator.min_value, rEstimator.max_value + 1):
+      se = sEstimator.estimate_equal(x)
+      re = rEstimator.estimate_equal(x)
+      errors.append(abs(se - re))
 
-  avg_classic_diff = sum(errors) / len(errors)
+    avg_classic_diff = sum(errors) / len(errors)
+    error_avgs_col.append(avg_classic_diff)
 
-  columns.append(n)
-  error_avgs.append(avg_classic_diff)
+  columns.append(par)
+  error_avgs.append(sum(error_avgs_col) / float(len(error_avgs_col)))
 
 
 import matplotlib.pyplot as plt
 
-b = plt.plot(columns, error_avgs, 'bo')
+b = plt.plot(columns, error_avgs, 'b^')
 # Pad margins so that markers don't get clipped by the axes
 plt.margins(0.2)
 # Tweak spacing to prevent clipping of tick-labels
 plt.subplots_adjust(bottom=0.15)
 plt.legend( (a[0], b[0]), ('Uniforme', 'Normal') )
 plt.ylabel('Error promedio')
-plt.xlabel('Numero de steps')
+plt.xlabel('Cantidad de steps')
 plt.show()
 
 # Aca por un error el en codigo al principio daban igual con todos los parameters.. (podriamos chamuyar algo en el informe
